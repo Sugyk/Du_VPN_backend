654c29f820951799825571508080bc756d9fde3c
Sugyk <active.sugyk@gmail.com>
2025-03-23 16:09:34 +0300

    refactor: add sslmode param to db connection

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/sugyk/rest_vpn
 
 go 1.24.1
 
-require github.com/jmoiron/sqlx v1.4.0
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
--- lib/database/connection.go
+++ lib/database/connection.go
@@ -4,24 +4,27 @@ import (
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 type ConfingDB struct {
 	Host     string
-	Port     int
+	Port     string
 	User     string
 	Password string
 	Name     string
+	SSLMode  string
 }
 
 func NewPostgresConnection(cnf ConfingDB) (*sqlx.DB, error) {
 	dsn := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		cnf.Host,
 		cnf.Port,
 		cnf.User,
 		cnf.Password,
 		cnf.Name,
+		cnf.SSLMode,
 	)
 	db, err := sqlx.Connect("postgres", dsn)
 	return db, err
