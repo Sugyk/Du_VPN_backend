539516db1e93896124ad2c1ba8b63adef52da458
Sugyk <active.sugyk@gmail.com>
2025-04-17 13:53:16 +0300

    feat: add check X-Auth-Token middleware

---
+++ api/middlewares/auth.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("AuthMiddleware: Checking auth token")
+		if r.Header.Get("X-Auth-Token") == "" {
+			http.Error(w, "Auth token is required", http.StatusUnauthorized)
+			return
+		}
+		if r.Header.Get("X-Auth-Token") != os.Getenv("AUTH_TOKEN") {
+			log.Println(r.Header.Get("X-Auth-Token"), os.Getenv("AUTH_TOKEN"))
+			http.Error(w, "Auth token is incorrect", http.StatusUnauthorized)
+			return
+		}
+		next(w, r)
+	}
+}
