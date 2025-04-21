41901c01ff9a35fd1b7eaacba3a8b7ced1438212
Sugyk <active.sugyk@gmail.com>
2025-04-21 14:31:01 +0300

    typo: set better param name

--- api/handlers/helpers.go
+++ api/handlers/helpers.go
@@ -6,13 +6,13 @@ import (
 	"net/http"
 )
 
-func getBody(r *http.Request, resp interface{}) error {
+func getBody(r *http.Request, dest interface{}) error {
 	defer r.Body.Close()
 	b, err := io.ReadAll(r.Body)
 	if err != nil {
 		return err
 	}
-	if err := json.Unmarshal(b, &resp); err != nil {
+	if err := json.Unmarshal(b, &dest); err != nil {
 		return err
 	}
 	return nil
