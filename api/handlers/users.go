31bf66923faf5b651b9464dd5bb24e258ab20f04
Sugyk <active.sugyk@gmail.com>
2025-05-22 16:15:03 +0300

    typo: clear comments in createuser handler

--- api/handlers/users.go
+++ api/handlers/users.go
@@ -8,9 +8,11 @@ import (
 	"github.com/sugyk/rest_vpn/repository"
 )
 
+// this handler create a new user, who is admin or not
+//
+//	200 - if user created or already in db
+//	500 - if something is wrong
 func (s *Service) CreateUser() http.HandlerFunc {
-	// This function will return a handler function
-	// to create a new key
 	type request struct {
 		Telegram_id int  `json:"telegram_id"`
 		Is_admin    bool `json:"is_admin"`
@@ -24,7 +26,7 @@ func (s *Service) CreateUser() http.HandlerFunc {
 			log.Println("Error parsing request body: ", err)
 		}
 
-		// Execute main business logic
+		// Create user in repo
 		err = s.Repo.CreateUser(repository.CreateUserParams{
 			Telegram_id: req_body.Telegram_id,
 			Is_admin:    req_body.Is_admin,
@@ -32,9 +34,11 @@ func (s *Service) CreateUser() http.HandlerFunc {
 
 		// Form response
 		if err != nil {
+			// 500
 			log.Println("Error of creating of user: ", err)
 			w.WriteHeader(http.StatusInternalServerError)
 		} else {
+			// 200
 			log.Println("User successfully created")
 			w.WriteHeader(http.StatusOK)
 			w.Write(fmt.Append([]byte{}, `{"message": "OK"}`))
