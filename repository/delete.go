c71c171458279fb9b788e5adbd096f50ef0d555c
Sugyk <active.sugyk@gmail.com>
2025-05-22 18:13:07 +0300

    feat: implement the delete handler in repo

---
+++ repository/delete.go
@@ -0,0 +1,21 @@
+package repository
+
+type DeleteKeyParams struct {
+	KeyId int `db:"id"`
+}
+
+// delete the AccessKey from repo by id
+func (r *Repository) DeleteKey(params DeleteKeyParams) error {
+	query := `
+	DELETE FROM "AccessKey"
+	WHERE id = :id
+	`
+
+	_, err := r.db.NamedExec(query, params)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
